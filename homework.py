4c6a8380c5d288217558eb4ffc947823de23d616
Egor <egor.kchr@mail.ru>
2020-10-09 15:18:26 +0300

    All tests passed. Errors fixed. Second commit

--- homework.py
+++ homework.py
@@ -1,52 +1,57 @@
 import datetime as dt
 
 class Calculator:
-    # Инициализация данных. 
     def __init__(self, limit):
+        """Инициализировать данные создаваемого объекта."""
         self.limit = limit
         self.records = []
 
-    # Метод для добавления записей.
     def add_record(self, record):
+        """Добавить запись."""
         self.records.append(record)
     
-    # Метод для вычисления затрат на текущий день.
     def get_today_stats(self):
-        today_sum = 0
-        for record in self.records:
-            if record.date == dt.datetime.now().date():
-                today_sum = today_sum + record.amount
+        """Возвращает затраты на текущий день."""
+        # Текущий день.
+        today = dt.date.today()
+        today_sum = sum([record.amount for record in self.records 
+                            if record.date == today])
         return today_sum
 
-    # Метод для вычисления затрат за неделю.
     def get_week_stats(self):
-        week_sum = 0
-        for record in self.records:
-            # Проверка на попадание записи в недельный интервал.
-            if (record.date > dt.datetime.now().date() - dt.timedelta(days=7)
-            and record.date <= dt.datetime.now().date()):
-                week_sum = week_sum + record.amount
+        """Возвращает затраты за неделю."""
+        # Текущий день.
+        today = dt.date.today()
+        # Недельный интервал.
+        week_interval = today - dt.timedelta(days=7)
+        week_sum = sum([record.amount for record in self.records 
+                            if week_interval < record.date <= today])
         return week_sum
+
+    def get_amount_left(self):
+        """Возвращает количество оставшихся ресурсов."""
+        # Количество потраченных ресурсов.
+        amount_spent = self.get_today_stats()
+        # Количество оставшихся ресурсов.
+        amount_left = self.limit - amount_spent
+        return amount_left
 
 
 class Record:
-    # Инициализация данных. 
-    def __init__(self, amount, comment, date=dt.datetime.today().date()):
+    def __init__(self, amount, comment, date=None):
+        """Инициализация данных."""
         self.amount = amount
         self.comment = comment
-        if type(date) == str:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+        if date is None:
+            self.date = dt.date.today()
         else:
-            self.date = date
+            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
 
 
 class CaloriesCalculator(Calculator):
-    # Метод для вычисления оставшихся каллорий.
     def get_calories_remained(self):
-        # Количество потраченных каллорий.
-        calories_spent = self.get_today_stats()
-        # Количество оставшихся каллорий.
-        calories_left = self.limit - calories_spent
+        """Возвращает информацию об оставшихся каллориях."""
+        calories_left = self.get_amount_left()
         if calories_left > 0:
             return ('Сегодня можно съесть что-нибудь ещё, но с общей '
                         f'калорийностью не более {calories_left} кКал')
@@ -58,27 +63,23 @@ class CashCalculator(Calculator):
     USD_RATE = 78.00
     EURO_RATE = 91.00
 
-    # Метод для вычисления оставшихся денег.
     def get_today_cash_remained(self, currency):
-        str_currency = ''
-        # Количество потраченных денег.
-        cash_spent = self.get_today_stats()
-        # Количество оставшихся денег.
-        cash_left = self.limit - cash_spent
-        # Определение валюты.
-        if currency == 'rub':
-            str_currency = 'руб'
-        elif currency == 'usd':
-            cash_left = round(cash_left / self.USD_RATE, 2)
-            str_currency = 'USD'
-        elif currency == 'eur':
-            cash_left = round(cash_left / self.EURO_RATE, 2)
-            str_currency = 'Euro'
-        else:
-            raise ValueError
-        if cash_left > 0:
-            return f'На сегодня осталось {cash_left} {str_currency}'
+        """Возвращает информацию об оставшихся деньгах."""
+        cash_left = self.get_amount_left()
         if cash_left == 0:
             return 'Денег нет, держись'
+        # Определение валюты.
+        RATE_DICT = {
+        'rub': ('руб', 1.00),
+        'usd': ('USD', CashCalculator.USD_RATE),
+        'eur': ('Euro', CashCalculator.EURO_RATE)
+        }
+        if currency in RATE_DICT:
+            cash_left = round(cash_left / RATE_DICT[currency][1], 2)
+        else:
+            raise ValueError
+        str_currency = RATE_DICT[currency][0]
+        if cash_left > 0:
+            return f'На сегодня осталось {cash_left} {str_currency}'
         cash_left = abs(cash_left)
         return f'Денег нет, держись: твой долг - {cash_left} {str_currency}'
